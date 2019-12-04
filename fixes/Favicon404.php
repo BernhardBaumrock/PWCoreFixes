c4fa410654d0d4a4f8a50c53315a350cdf60d347
BernhardBaumrock <bernhard@baumrock.com>
2019-12-04 13:36:59 +0100

    add favicon fix

---
+++ fixes/Favicon404.php
@@ -0,0 +1,18 @@
+<?php namespace ProcessWire;
+class Favicon404 extends PWFix {
+  public $label = "Prevent Favicon 404 error";
+  public $description = "If no favicon.ico file exists, devtools will report a 404. This fix will add a dummy favicon so that the 404 error is not displayed in the devtools.";
+  public function init() {
+    $this->addHookAfter('Page::render', function($event) {
+      $file = $this->config->paths->root."favicon.ico";
+      if(is_file($file)) return;
+      $html = $event->return;
+      $html = str_replace(
+        "</head>",
+        '<link rel="icon" href="data:;base64,iVBORw0KGgo="></head>',
+        $html
+      );
+      $event->return = $html;
+    });
+  }
+}
