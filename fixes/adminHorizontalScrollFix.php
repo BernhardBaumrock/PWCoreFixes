4db6b4659692e3888a20ea89752fdce40d4425fe
BernhardBaumrock <office@baumrock.com>
2020-03-05 09:51:01 +0100

    add horizontal scrollbar fix

---
+++ fixes/adminHorizontalScrollFix.php
@@ -0,0 +1,8 @@
+<?php namespace ProcessWire;
+class adminHorizontalScrollFix extends PWFix {
+  public $label = "Fix horizontal scrollbar in admin";
+  public $description = "Please see the Github issue";
+  public $issue = "https://github.com/processwire/processwire-issues/issues/812";
+  public $author = "matjazpotocnik"; // github user
+  public function init() {}
+}
