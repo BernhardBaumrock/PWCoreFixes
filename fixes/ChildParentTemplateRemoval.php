577cc5cf01c748f66328658ae0b5098937f62d6f
BernhardBaumrock <office@baumrock.com>
2020-04-15 11:49:58 +0200

    add author

--- fixes/ChildParentTemplateRemoval.php
+++ fixes/ChildParentTemplateRemoval.php
@@ -3,9 +3,9 @@ class ChildParentTemplateRemoval extends PWFix {
   public $label = "childTemplates and parentTemplates removal";
   public $description = "Removes a template from all template's `childTemplates` and `parentTemplates` settings when template itself is deleted.";
   public $issue = 'https://github.com/processwire/processwire-issues/issues/802';
+  public $author = 'adrianbj';
   public function init() {
     $this->addHookAfter('Templates::delete', function($event) {
-      bdb($event->arguments[0]->name);
       $templates = $event->object;
       foreach($templates as $t) {
           // child templates
