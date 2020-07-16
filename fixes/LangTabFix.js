1635d6cf10191e92232983092e9034e4fee923f3
BernhardBaumrock <office@baumrock.com>
2020-07-16 14:27:40 +0200

    support repeater fields

--- fixes/LangTabFix.js
+++ fixes/LangTabFix.js
@@ -1,9 +1,17 @@
 $(document).ready(function(e) {
-  if(typeof ProcessWire.config.LanguageTabs == 'undefined') return;
-  let lang = ProcessWire.config.LanguageTabs.activeTab;
-  if(!lang) return;
-  let $tabs = $('div.langTabs > ul');
-  $.each($tabs, function(i, tab) {
-    UIkit.tab(tab).show(lang);
-  });
+  let initTabs = function() {
+    if(typeof ProcessWire.config.LanguageTabs == 'undefined') return;
+    let lang = ProcessWire.config.LanguageTabs.activeTab;
+    if(!lang) return;
+    let $tabs = $('div.langTabs > ul');
+    $.each($tabs, function(i, tab) {
+      UIkit.tab(tab).show(lang);
+    });
+  }
+
+  // init tabs on page load
+  initTabs();
+
+  // init tabs after ajax requests (eg repeater fields)
+  $(document).ajaxComplete(initTabs);
 });
